f7cfaabea54e4db88e7252f519a9b0bb8692a6b2
lelouch-17 <nishanttorres94@gmail.com>
2023-08-17 18:24:28 +0530

    remove space from index

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = 4000; // Change this if needed
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 }); 
